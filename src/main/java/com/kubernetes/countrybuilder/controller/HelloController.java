b646460108040dccc9ead44a9dfcb0cc280a8cb8
Arun <arunachalam944@gmail.com>
2021-10-16 02:38:50 -0700

    Kubernetes Secret

--- src/main/java/com/kubernetes/countrybuilder/controller/HelloController.java
+++ src/main/java/com/kubernetes/countrybuilder/controller/HelloController.java
@@ -1,6 +1,7 @@
 package com.kubernetes.countrybuilder.controller;
 
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,13 +9,23 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/hello")
+@Slf4j
 public class HelloController {
 
     @Value("${country:England}")
     private String country;
 
+    @Value("${secretcode}")
+    private String secretCode;
+
     @GetMapping("/hi")
     public String returnmessage(){
         return new String(country);
+    }
+
+    @GetMapping("/secret")
+    public String returnSecret(){
+        log.info("secret is: {}",secretCode);
+        return secretCode;
     }
 }
